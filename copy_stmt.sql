dffe4990a2b6f8a7fc2f2cea92d5bbd6a1e437c1
sjaanm <sjaan.maike@arnsfeld.net>
2017-06-10 15:06:50 +0200

    Create copy_stmt.sql

--- copy_stmt.sql
+++ copy_stmt.sql
@@ -1,4 +1,4 @@
-﻿COPY visitor (visitor_id, email, last_name, first_name, address, country, birthdate, phone, sex)
+COPY visitor (visitor_id, email, last_name, first_name, address, country, birthdate, phone, sex)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/visitor.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
@@ -20,8 +20,32 @@ COPY ticket_price (ticket_price_id, price, valid_from, valid_to, ticket_type_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/ticket_price.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
+
+
+COPY location (location_id, postcode, city, country)
+FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/location.csv'
+WITH DELIMITER ';'
+NULL AS 'null' csv;
  
  
+COPY festival_event (festival_event_id, start_date, end_date, name, location_id)
+FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/festival_event.csv'
+WITH DELIMITER ';'
+NULL AS 'null' csv;
+ 
+
+COPY application (application_id, type, description, date, status, festival_event_id)
+FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/application.csv'
+WITH DELIMITER ';'
+NULL AS 'null' csv;
+  
+ 
+COPY provider (provider_id, name, application_id)
+FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/provider.csv'
+WITH DELIMITER ';'
+NULL AS 'null' csv;
+
+
 COPY sponsor (sponsor_id, provider_id, money)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/sponsor.csv'
 WITH DELIMITER ';'
@@ -32,38 +56,38 @@ COPY vendor (vendor_id, sponsor_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/vendor.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY donor (donor_id, sponsor_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/donor.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY area (area_type, capacity)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/area.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
  
- 
+
 COPY advertisement (advertisement_id, type, quantity, donor_id, area_type)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/advertisement.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
+
  
 COPY stage (stage_id, name, capacity, type, number_seats)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/stage.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY band (band_id, headliner, timeslot_date, timeslot_start, timeslot_end, press_information, is_cancelled, provider_id, stage_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/band.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY song (song_id, name, lyricist, length)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/song.csv'
 WITH DELIMITER ';'
@@ -74,20 +98,20 @@ COPY visitor_account (username, password, filter_date, alarm, visitor_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/visitor_account.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY timetable ( timetable_id, band_id, username, rating)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/timetable.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY product (product_id, name, price, type, category, provider_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/product.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
  
- 
+
 COPY shop (shop_id, name, category, vendor_id, area_type)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/shop.csv'
 WITH DELIMITER ';'
@@ -98,22 +122,10 @@ COPY sold_in (shop_id, product_id)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/sold_in.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
- 
- 
+
+
 COPY wristband (wristband_id, visitor_id, disabled, balance)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/wristband.csv'
-WITH DELIMITER ';'
-NULL AS 'null' csv;
- 
- 
-COPY location (location_id, postcode, city, country)
-FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/location.csv'
-WITH DELIMITER ';'
-NULL AS 'null' csv;
- 
- 
-COPY festival_event (festival_event_id, start_date, end_date, name, location_id)
-FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/festival_event.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
      
@@ -170,6 +182,12 @@ COPY newsletter (newsletter_id, title, publish_time)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/newsletter.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
+
+
+COPY newsletter_application (visitor_id, newsletter_id)
+FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/newsletter_application.csv'
+WITH DELIMITER ';'
+NULL AS 'null' csv;
  
  
 COPY post (post_id, heading, content, publish_time, tags, newsletter_id)
@@ -183,17 +201,6 @@ FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/sale.csv'
 WITH DELIMITER ';'
 NULL AS 'null' csv;
  
- 
-COPY application (application_id, type, description, date, status, festival_event_id)
-FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/application.csv'
-WITH DELIMITER ';'
-NULL AS 'null' csv;
-  
- 
-COPY provider (provider_id, name, application_id)
-FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/provider.csv'
-WITH DELIMITER ';'
-NULL AS 'null' csv;
  
 COPY area_access (wristband_id, area_type, time)
 FROM '/home/sj/Schreibtisch/Datenbanksysteme/project_tables/area_access.csv'
