6b5c90184fbe013b047427dd307f06c4c2a7f501
Vishesh-DBPra <vmath89@gmail.com>
2017-07-11 15:48:09 +0200

    Merge branch 'master' of https://github.com/kirillkd/festival

---
+++ src/beans/ProviderBean.java
@@ -0,0 +1,23 @@
+package beans;
+
+public class ProviderBean {
+
+	private int provider_id;
+	private String name;
+	
+	public int getProvider_id() {
+		return provider_id;
+	}
+	public void setProvider_id(int provider_id) {
+		this.provider_id = provider_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
