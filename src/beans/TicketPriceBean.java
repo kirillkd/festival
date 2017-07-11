e99fe0a66f9fb63c1bfa416b4cef31291ab0d2a5
sjaanm <sjaan.maike@arnsfeld.net>
2017-07-11 14:03:13 +0200

    Adding TicketPriceBean

--- src/beans/TicketPriceBean.java
+++ src/beans/TicketPriceBean.java
@@ -1,12 +1,11 @@
 package beans;
-import java.util.Date;
 
 public class TicketPriceBean {
+
 	private int ticket_price_id;
 	private double price;
-	private Date valid_from;
-	private Date valid_to;
-	private int ticket_type_id;
+	private String valid_from;
+	private String valid_to;
 	
 	public int getTicket_price_id() {
 		return ticket_price_id;
@@ -20,23 +19,17 @@ public class TicketPriceBean {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	public Date getValid_from() {
+	public String getValid_from() {
 		return valid_from;
 	}
-	public void setValid_from(Date valid_from) {
+	public void setValid_from(String valid_from) {
 		this.valid_from = valid_from;
 	}
-	public Date getValid_to() {
+	public String getValid_to() {
 		return valid_to;
 	}
-	public void setValid_to(Date valid_to) {
+	public void setValid_to(String valid_to) {
 		this.valid_to = valid_to;
-	}
-	public int getTicket_type_id() {
-		return ticket_type_id;
-	}
-	public void setTicket_type_id(int ticket_type_id) {
-		this.ticket_type_id = ticket_type_id;
 	}
 	
 	
