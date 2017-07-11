d12a838938ea8253eb5413217720d9356d52d243
Mangosteen <mangosteen@users.noreply.github.com>
2017-07-11 14:45:35 +0200

    Sold_InBean added

--- src/beans/ProductBean.java
+++ src/beans/ProductBean.java
@@ -2,8 +2,38 @@ package beans;
 
 public class ProductBean {
 
-	public ProductBean() {
-		// TODO Auto-generated constructor stub
+	private int product_id;
+	private String name;
+	private double price;
+	private String type;
+	private enum product_category{
+		beverage, food, clothes, accessory
 	}
+	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	};
+
 
 }
