c61b63259ae69de4f970de4aa24b96375653f78c
Mangosteen <mangosteen@users.noreply.github.com>
2017-07-11 14:20:28 +0200

    BandBean added

---
+++ src/beans/BandBean.java
@@ -0,0 +1,9 @@
+package beans;
+
+public class BandBean {
+
+	public BandBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/beans/TicketBean.java
+++ src/beans/TicketBean.java
@@ -1,9 +1,36 @@
 package beans;
 
+import java.util.Date;
+
 public class TicketBean {
 
-	public TicketBean() {
-		// TODO Auto-generated constructor stub
-	}
+		private int ticket_id;
+		private Date booking_date;
+		private String payment_method;
+		
+		public int getTicket_id() {
+			return ticket_id;
+		}
+		public void setTicket_id(int ticket_id) {
+			this.ticket_id = ticket_id;
+		}
+		public Date getBooking_date() {
+			return booking_date;
+		}
+		public void setBooking_date(Date booking_date) {
+			this.booking_date = booking_date;
+		}
+		public String getPayment_method() {
+			return payment_method;
+		}
+		public void setPayment_method(String payment_method) {
+			this.payment_method = payment_method;
+		}
+		
+		
+	
+	
 
 }
+
+
