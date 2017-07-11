7786864dfdb8eb2910d93274ba1c6ad0449b46bf
sjaanm <sjaan.maike@arnsfeld.net>
2017-07-11 14:33:03 +0200

    Adding ShopBean

---
+++ src/beans/ShopBean.java
@@ -0,0 +1,31 @@
+package beans;
+
+public class ShopBean {
+
+	private int shop_id;
+	private String name;
+	private String category;
+	
+	public int getShop_id() {
+		return shop_id;
+	}
+	public void setShop_id(int shop_id) {
+		this.shop_id = shop_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+
+	
+}
--- src/beans/VisitorAccountBean.java
+++ src/beans/VisitorAccountBean.java
@@ -1,5 +1,39 @@
 package beans;
 
+import java.util.Date;
+
 public class VisitorAccountBean {
 
+	private String username;
+	private String password;
+	private Date filter_date;
+	private Date alarm;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getFilter_date() {
+		return filter_date;
+	}
+	public void setFilter_date(Date filter_date) {
+		this.filter_date = filter_date;
+	}
+	public Date getAlarm() {
+		return alarm;
+	}
+	public void setAlarm(Date alarm) {
+		this.alarm = alarm;
+	}
+	
+	
+	
 }
