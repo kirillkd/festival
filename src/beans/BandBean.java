7ef1ba8f8817bd9438ecf6023fdce52eb3157f87
kechtel <info@christoph-kecht.de>
2017-07-24 20:07:41 +0200

    Remove unused import

--- src/beans/BandBean.java
+++ src/beans/BandBean.java
@@ -2,9 +2,8 @@ package beans;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Date;
 
-public class BandBean extends ProviderBean{
+public class BandBean extends ProviderBean {
 
 	private int band_id;
 	private boolean headliner;
@@ -13,51 +12,60 @@ public class BandBean extends ProviderBean{
 	private LocalTime timeslot_end;
 	private String press_information;
 	private boolean is_cancelled;
-	
+
 	public int getBand_id() {
 		return band_id;
 	}
+
 	public void setBand_id(int band_id) {
 		this.band_id = band_id;
 	}
+
 	public boolean isHeadliner() {
 		return headliner;
 	}
+
 	public void setHeadliner(boolean headliner) {
 		this.headliner = headliner;
 	}
+
 	public LocalDate getTimeslot_date() {
 		return timeslot_date;
 	}
+
 	public void setTimeslot_date(LocalDate timeslot_date) {
 		this.timeslot_date = timeslot_date;
 	}
+
 	public LocalTime getTimeslot_start() {
 		return timeslot_start;
 	}
+
 	public void setTimeslot_start(LocalTime timeslot_start) {
 		this.timeslot_start = timeslot_start;
 	}
+
 	public LocalTime getTimeslot_end() {
 		return timeslot_end;
 	}
+
 	public void setTimeslot_end(LocalTime timeslot_end) {
 		this.timeslot_end = timeslot_end;
 	}
+
 	public String getPress_information() {
 		return press_information;
 	}
+
 	public void setPress_information(String press_information) {
 		this.press_information = press_information;
 	}
+
 	public boolean isIs_cancelled() {
 		return is_cancelled;
 	}
+
 	public void setIs_cancelled(boolean is_cancelled) {
 		this.is_cancelled = is_cancelled;
 	}
-	
-	
 }
-	
-
